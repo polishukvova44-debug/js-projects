c88048e8fda59befb2efcc5c86a5758eae767b85
polishukvova44-debug <polishukvova44@gmail.com>
2025-11-15 14:58:14 +0200

    Create password-generator.js

---
+++ password-generator.js
@@ -0,0 +1,13 @@
+function generatePassword(length) {
+    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let password = "";
+    
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        password += chars[randomIndex];
+    }
+
+    return password;
+}
+
+console.log("Generated password:", generatePassword(12));
